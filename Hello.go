aba5aa31d7f011d2ec7f19b5f1179474b69c8a5a
David Ilenwabor <dave11ilen@gmail.com>
2019-10-10 02:24:07 +0100

    Latest

--- Hello.go
+++ Hello.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello world, welcome to my Go\n")
+	//statically typed, you must use all variables
+	// var age int = 40
+	// var newFloat float32 = 1.64
+	var newString = "This is my new string"
+	var stringLength int = len(newString)
+	const pi float32 = 3.142
+	fmt.Println("String length is "+newString+" with length ", stringLength)
+
+	i := 1
+
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
 }
